9d7e40c6cd6bcde1d2c2863394d48417817d9e12
sipasi <tanksasha77@gmail.com>
2024-02-07 12:46:00 +0200

    feat: icon control

---
+++ src/view/shared/icon/Icons.ts
@@ -0,0 +1,8 @@
+enum Icons {
+  calendar_month = "calendar_month",
+  list_alt = "list_alt",
+  settings = "settings",
+  add = "add",
+}
+
+export default Icons;
---
+++ src/view/shared/icon/SvgIcon.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Icons from "./Icons";
+
+  export let type: Icons;
+</script>
+
+<img src="icons/{type}.svg" alt="" />
+
+<style>
+  img {
+    filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(160deg)
+      brightness(101%) contrast(105%);
+
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+</style>
