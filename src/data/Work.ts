2a78e0e5c042257c0d3f2a0f52e9b8eb2cb68b62
sipasi <tanksasha77@gmail.com>
2024-02-13 00:50:39 +0200

    feat: remake work model

--- src/data/Work.ts
+++ src/data/Work.ts
@@ -1,23 +1,12 @@
-import CalculationType from "./CalculationType";
+import type WorkType from "./WorkType";
 
 class Work {
-  public readonly price: number;
-
-  public readonly type: string;
-
-  public readonly calculation: CalculationType;
+  public readonly type: WorkType;
 
   public readonly units: Array<string>;
 
-  constructor(
-    price: number,
-    type: string,
-    calculation: CalculationType,
-    units: Array<string>
-  ) {
-    this.price = price;
+  constructor(type: WorkType, units: Array<string>) {
     this.type = type;
-    this.calculation = calculation;
     this.units = units;
   }
 }
